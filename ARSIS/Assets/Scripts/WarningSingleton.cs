4e2e0e542cf955ae1546b437051fb8b1d8f2b638
Unknown <dean3712@gmail.com>
2019-04-17 22:57:07 -0500

    Fixed biometric warning bug

--- ARSIS/Assets/Scripts/WarningSingleton.cs
+++ ARSIS/Assets/Scripts/WarningSingleton.cs
@@ -8,20 +8,35 @@ public class WarningSingleton : MonoBehaviour
 
     [HideInInspector]
     public bool m_DataInWarning = false;
+
+    private List<string> m_Warnings;
     // Start is called before the first frame update
     void Awake()
     {
         m_Singleton = this;
+        m_Warnings = new List<string>();
     }
 
-    public void BiometricInWarning()
+    public void BiometricInWarning(string s)
     {
+        if (m_Warnings.Contains(s)) return;
+
+        m_Warnings.Add(s);
         m_DataInWarning = true;
         FindObjectOfType<OutputErrorData>().OutputErrorText("Warning in Biometrics");
     }
 
-    public void BiometricInNominal()
+    public void BiometricInNominal(string s)
     {
-        m_DataInWarning = false;
+        if (m_Warnings.Contains(s))
+        {
+            m_Warnings.Remove(s);
+        }
+
+        if (m_Warnings.Count == 0)
+        {
+            m_DataInWarning = false;
+        }
+
     }
 }
